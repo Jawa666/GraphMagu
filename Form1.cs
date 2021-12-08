1bf049acb27c641329453dfe201eb7ed5afa7837
Anton Lukyanchenko <super.tankes@yandex.ru>
2021-12-08 14:28:20 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Kursovaya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // некоторые глобальные переменные 
+        int graphValue;
+
+        private void button1_Click(object sender, EventArgs e) //Кнопка Ввести
+        {
+            try
+            {
+                graphValue = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректная запись","Сообщение",MessageBoxButtons.OK);
+            }
+
+            int[,] graphMatrix = new int[graphValue, graphValue];  //Сгенерировал матрицу нулями
+            for (int i = 0; i < graphValue; i++)
+                for (int j = 0; j < graphValue; j++)
+                    graphMatrix[i, j] = 0;
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            for(int i = 0; i < graphValue+1; i++)
+            {
+                if (i != 0 )
+                {
+                    dataGridView1.Columns.Add(Convert.ToString(Convert.ToChar(64 + i)), Convert.ToString(Convert.ToChar(64 + i)));
+                    dataGridView1.Columns[i].Width = 30;
+                    if(i != graphValue)
+                        dataGridView1.Rows.Add();
+                }
+                else
+                {
+                    dataGridView1.Columns.Add(Convert.ToString(Convert.ToChar(64 + i)), "");
+                    dataGridView1.Columns[i].Width = 30;
+                    if (i != graphValue)
+                        dataGridView1.Rows.Add();
+                }
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)              //по числу ячейк вроде норм, но при обращении к ячейке заменил i,j местами
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    if (j == 0) dataGridView1[j, i].Value = Convert.ToString(Convert.ToChar(65 + i));
+                    else dataGridView1[j, i].Value = "0";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)   //Кнопка (Метод магу)
+        {
+            int[,] graphMatrix = new int[graphValue, graphValue];  //Сгенерировал матрицу нулями
+            for (int i = 0; i < graphValue; i++)
+                for (int j = 0; j < graphValue; j++) 
+                    graphMatrix[i, j] = int.Parse(Convert.ToString( dataGridView1[j+1,i].Value ));
+            //Проверки класа днф
+            DNF dnf = new DNF();
+            List<string> list = dnf.DNFMagu(graphMatrix);
+            // берем значения к хрому
+            HromMagu hr = new HromMagu();
+            List<List<string>> HromChislo = hr.Hrom(list, graphMatrix.GetLength(0));
+            label7.Text = Convert.ToString(HromChislo[0].Count());  //вывод хроматического числа.
+
+            label5.Text = Convert.ToString(list.Count());
+
+            for (int i = 0; i < list.Count(); i++)
+                if(i == 0)
+                    textBox2.Text = ("S" + (i + 1) + " = {" + list[i] + "};");
+                else if(i != list.Count()-1)
+                    textBox2.Text += Environment.NewLine + ("S"+(i+1)+" = {"+list[i]+"};");
+                else
+                    textBox2.Text += Environment.NewLine + ("S" + (i+1) + " = {" + list[i] + "}.");
+
+
+            //Ввывод хроматичексих чисел
+            for(int i = 0; i < HromChislo.Count(); i++)
+            {
+                string str = "";
+                for(int j = 0; j < HromChislo[i].Count(); j++)
+                {
+                    if (j == 0)
+                        str = str + "S" + (i + 1) + " = {" + HromChislo[i][j] + ", ";
+                    else if (j == HromChislo[i].Count() - 1)
+                        str = str + HromChislo[i][j] + "}.";
+                    else
+                        str = str + HromChislo[i][j] + ", ";
+                }
+                if (i == 0)
+                    textBox3.Text = str;
+                else
+                    textBox3.Text += Environment.NewLine + str;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)   // Кнопка (Случайный)
+        {
+            try
+            {
+                graphValue = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректная запись", "Сообщение", MessageBoxButtons.OK);
+            }
+
+            int[,] graphMatrix = new int[graphValue, graphValue];  //Сгенерировал матрицу нулями
+            for (int i = 0; i < graphValue; i++)
+                for (int j = 0; j < graphValue; j++)
+                    graphMatrix[i, j] = 0;
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < graphValue + 1; i++)
+            {
+                if (i != 0)
+                {
+                    dataGridView1.Columns.Add(Convert.ToString(Convert.ToChar(64 + i)), Convert.ToString(Convert.ToChar(64 + i)));
+                    dataGridView1.Columns[i].Width = 30;
+                    if (i != graphValue)
+                        dataGridView1.Rows.Add();
+                }
+                else
+                {
+                    dataGridView1.Columns.Add(Convert.ToString(Convert.ToChar(64 + i)), "");
+                    dataGridView1.Columns[i].Width = 30;
+                    if (i != graphValue)
+                        dataGridView1.Rows.Add();
+                }
+            }
+
+            var rnd = new Random();
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)              //по числу ячейк вроде норм, но при обращении к ячейке заменил i,j местами
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    if (j == 0) dataGridView1[j, i].Value = Convert.ToString(Convert.ToChar(65 + i));
+                    else
+                    {
+                        int r1 = rnd.Next(101);
+                        if (r1 < 19 && i != (j-1))
+                            dataGridView1[j, i].Value = "1";
+                        else
+                            dataGridView1[j, i].Value = "0";
+                    }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[,] graphMatrix = new int[graphValue, graphValue];  
+            for (int i = 0; i < graphValue; i++)
+                for (int j = 0; j < graphValue; j++)
+                    graphMatrix[i, j] = int.Parse(Convert.ToString(dataGridView1[j + 1, i].Value));
+            
+            Graph graph = new Graph(graphMatrix);
+            graph.Show();
+
+
+            
+        }
+    }
+}
---
+++ HromMagu.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursovaya
+{
+    class HromMagu
+    {
+        public List<List<string>> Hrom(List<string> magu, int vershina)
+        {
+            List<string> listMagu = magu;
+            List<string> ishod = NahojdenieDiz(magu, vershina);//развертывание в днф
+            List<string> nedostatok = Nedostat(ishod, vershina);//нахождение слагаемых в ввиде цифр
+            List<List<string>> listConvert = Convertt(nedostatok); // конвертанция в буквенные представления где элементы 
+                                                                   //list[i]-это коньюкция, а элементы list[i][j]- дизъюнкции    
+            DNF df = new DNF();
+            listConvert = df.umnojenie(listConvert);
+            listConvert[0] = df.delOdinakovSimvolovVStroke(listConvert[0]);
+            listConvert[0] = df.poglaychenie(listConvert[0]);  //определили все слогаемые 
+            listMagu = listConvert[0];
+
+            List<string> minValue = nahojdenieMin(listMagu);
+            List<List<string>> result = HromGraph(minValue, magu);
+            
+            return result;
+        } 
+        public List<string> NahojdenieDiz(List<string> list, int value)  //получаем устойчивые множества и разворачиваем их в дизъюнкции
+        {
+            List<string> mnojestva = new List<string>();
+
+            SortedSet<char> vershini = new SortedSet<char>();
+            for (int i = 0; i < value; i++)
+                vershini.Add(Convert.ToChar(65 + i));
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                string mnojestvo = list[i];
+                SortedSet<char> vershina = new SortedSet<char>();
+
+                for (int j = 0; j < mnojestvo.Count(); j++)
+                    vershina.Add(mnojestvo[j]);
+
+                IEnumerable<char> mnojestvo2 = vershini.Except(vershina);
+                string elementStr2 = "";
+                foreach (char a in mnojestvo2)
+                    elementStr2 = elementStr2 + a;
+                mnojestva.Add(elementStr2);
+
+            }
+            return mnojestva;
+        }
+        public List<string> Nedostat(List<string> list, int value) //метод формирующий конъюкции y
+        {
+            List<string> result = new List<string>();
+
+            for(int i = 0; i < value; i++)
+            {
+                List<int> number = new List<int>();
+                string stroka = "";
+                for (int j = 0; j < list.Count(); j++)
+                {                   
+                    bool flag = false;
+                    string str = list[j];
+                    for(int q = 0; q < str.Count(); q++)
+                        if (Convert.ToChar(65 + i) == str[q]) 
+                            flag = true;
+
+                    if (!flag) number.Add(j);
+                }
+
+                for (int t = 0; t < number.Count(); t++)
+                    stroka = stroka + Convert.ToString(number[t]);
+                if(stroka.Count()!= 0)
+                    result.Add(stroka);
+            }
+
+            return result;
+        }
+        public List<List<string>> Convertt(List<string> list) //конвертируем числа в буквы
+        {
+            List<List<string>> result = new List<List<string>>();
+            for (int i = 0; i < list.Count(); i++)
+                result.Add(new List<string>());
+            for(int i = 0; i < list.Count; i++)
+            {
+                string str = list[i];
+                for(int j = 0; j < str.Length; j++)
+                {
+                    string a = "";
+                    a = a + str[j];
+                    result[i].Add(Convert.ToString(Convert.ToChar(65 + int.Parse(a))));
+                }
+            }
+            return result;
+        }
+        public List<string> nahojdenieMin(List<string> list)
+        {
+            List<string> result = new List<string>();
+            int[] mas = new int[list.Count()];
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = list[i].Count()-1;
+            int min = mas.Min();
+            for (int i = 0; i < mas.Length; i++)
+                if (mas[i] == min) result.Add(list[i]);
+
+            return result;
+        }
+        public List<List<string>> HromGraph(List<string> list, List<string> magu)
+        {
+            List<List<string>> result = new List<List<string>>();
+            for (int i = 0; i < list.Count(); i++)
+                result.Add(new List<string>());
+
+            List<string> chisla = new List<string>();
+            // сконвертируем данные на численные представления
+            for (int i = 0; i < list.Count(); i++)
+            {
+                string str = list[i];
+                string str2 = "";
+                for(int j = 0; j < list[i].Length; j++)           
+                    str2 += Convert.ToString(  ( (Convert.ToInt32(str[j])) - 65));
+                chisla.Add(str2);
+            }
+
+            //сопоставляем 
+            
+            for (int i = 0; i < chisla.Count(); i++)
+            {                                                   //с кода 48 начинатеся 0, с кода 65 начинается английская А
+                SortedSet<char> vershini = new SortedSet<char>();
+                string str = chisla[i];
+                for (int j = 0; j < str.Length; j++)
+                {
+                    SortedSet<char> prom = new SortedSet<char>();    //промежуточное множество
+                    int index = (Convert.ToInt32(Convert.ToChar(str[j]))) - 48; //нахождение мн-ва из листа магу
+
+                    string str2 = magu[index];
+                    for (int t = 0; t < str2.Length; t++)
+                        prom.Add(str2[t]);
+
+                    IEnumerable<char> element = prom.Except(vershini);
+                    string elementStr = "";
+                    foreach (char a in element)
+                        elementStr = elementStr + a;
+
+                    result[i].Add(elementStr);
+
+                    foreach (char a in prom)
+                        vershini.Add(a);
+                    
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
